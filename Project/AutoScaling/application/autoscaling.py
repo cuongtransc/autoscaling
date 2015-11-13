0ab919c8fcd084bb947a5ae8691124a039b0262d
Nguyen Ba Cuong <cuongnb14@gmail.com>
2015-11-13 15:39:46 +0700

    update version autoscaling ver 2 - add moniter memory

--- Project/AutoScaling/application/autoscaling.py
+++ Project/AutoScaling/application/autoscaling.py
@@ -15,140 +15,185 @@ from marathon import MarathonClient
 import sys
 import logging
 
-client = InfluxDBClient(INFLUXDB["host"], INFLUXDB["port"], INFLUXDB["username"], INFLUXDB["password"], INFLUXDB["db_name"])
-marathon_client = MarathonClient('http://'+MARATHON['host']+':'+MARATHON['port'])
+client = InfluxDBClient(config["INFLUXDB"]["host"], config["INFLUXDB"]["port"], config["INFLUXDB"]["username"], config["INFLUXDB"]["password"], config["INFLUXDB"]["db_name"])
+marathon_client = MarathonClient('http://'+config["MARATHON"]['host']+':'+config["MARATHON"]['port'])
 
 app = None
 number_instance = 0
+app_mem = 0
+app_cpus = 0
 
-logger = logging.getLogger("autoscaling")
-logger.setLevel(logging.DEBUG)
 
-def get_cpu_usage(container_name):
-	"""Return cpu usage of container_name
+class Scaler:
+	"""Class for Scaling"""
+	def __init__(self, app_name, config):
+		self.logger = logging.getLogger("autoscaling")
+		logger.setLevel(logging.DEBUG)
 
-	@param string container_name container name  
-	"""
-	query = "select DERIVATIVE(cpu_cumulative_usage)  as cpu_usage from stats where container_name = '"+container_name+"' and time > now()-5m group by time(10s) "
-	result = client.query(query)
-	points = result[0]["points"]
-	return points[0][1]/1000000000/4*100
+		logger.info("Init object scaler...")
+		self.config = config
 
-def get_container_name(mesos_task_id):
-	"""Return container name mapping with mesos_task_id in messos
-	
-	@param string mesos_task_id
-	"""
-	query = "select container_name from "+INFLUXDB["ts_mapping"]+" where time>now() - 5m and mesos_task_id = '" +mesos_task_id+"' limit 1" 
-	result = client.query(query)
-	points = result[0]["points"]
-	return points[0][2]
+		logger.info("Connect RESTful mariadb and get policies...")
+		conn = http.client.HTTPConnection(config["MARIA_RESTFUL"]['host'], config["MARIA_RESTFUL"]['port'])
+		conn.request("GET", "/app/name/"+app_name)
+		json_app = conn.getresponse().read().decode("utf-8")
+		self.app = json.loads(json_app)
+		conn.request("GET", "/app/name/"+app_name+"/policies")
+		json_policies = conn.getresponse().read().decode("utf-8")
+		self.app["policies"] = json.loads(json_policies)
 
-def get_containers_name(app_name):
-	"""Return list all containers name of application have name app_name
-	
-	@param string app_name name of application
-	@return list all containers name of app_name
-	"""
-	tasks = marathon_client.list_tasks(app_name)
-	containers_name = []
-	for task in tasks:
-		containers_name.append(get_container_name(task.id))
-	return containers_name
+		logger.info("Connect influxdb and marathon...")
+		self.influx_client = InfluxDBClient(config["INFLUXDB"]["host"], config["INFLUXDB"]["port"], config["INFLUXDB"]["username"], config["INFLUXDB"]["password"], config["INFLUXDB"]["db_name"])
+		self.marathon_client = MarathonClient('http://'+config["MARATHON"]['host']+':'+config["MARATHON"]['port'])
+		
+		self.app["instance"] = marathon_client.get_app(app_name).instances
+		self.app["mem"] = marathon_client.get_app(app_name).mem
+		self.app["cpus"] = marathon_client.get_app(app_name).cpus
 
-def avg_cpu_usage(containers_name):
-	"""Return avg cpu usage of all containers in list containers_name
-	
-	@param list containers_name list containers name
-	@return float avg cpu usage
-	"""
-	number_container = len(containers_name)
-	containers_name = ["'"+x+"'" for x in containers_name]
-	containers_name = ",".join(containers_name)
-	query = "select DERIVATIVE(cpu_cumulative_usage)  as cpu_usage,container_name from stats where  time > now()-5m and  container_name in ("+containers_name+") group by time(10s),container_name limit "+str(number_container)
-	result = client.query(query)
-	points = result[0]["points"]
-	return points[0][1]/1000000000/4*100
+		
 
-	sum_cpu_usage = 0
-	for point in points:
-		sum_cpu_usage += points[0][1]/1000000000/4*100
-	return sum_cpu_usage / number_container
+		logger.info("Reconfig haproxy.cfg...")
+		os.system("sudo ./servicerouter.py --marathon http://"+config["MARATHON"]["host"]+":"+config["MARATHON"]["port"]+" --haproxy-config /etc/haproxy/haproxy.cfg")
 
-def scale(app_name, delta):
+		
+
+	def get_cpu_usage(self, container_name):
+		"""Return cpu usage of container_name
+
+		@param string container_name container name  
+		"""
+		query = "select DERIVATIVE(cpu_cumulative_usage)  as cpu_usage from stats where container_name = '"+container_name+"' and time > now()-5m group by time(10s) "
+		result = self.influx_client.query(query)
+		points = result[0]["points"]
+		return (points[0][1]/1000000000/self.app["cpus"])*100
+
+	def get_container_name(self, mesos_task_id):
+		"""Return container name mapping with mesos_task_id in messos
+		
+		@param string mesos_task_id
+		"""
+		query = "select container_name from "+config["INFLUXDB"]["ts_mapping"]+" where time>now() - 5m and mesos_task_id = '" +mesos_task_id+"' limit 1" 
+		result = self.influx_client.query(query)
+		points = result[0]["points"]
+		return points[0][2]
+
+	def get_containers_name(self):
+		"""Return list all containers name of application have name app_name
+		
+		@param string app_name name of application
+		@return list all containers name of app_name
+		"""
+		tasks = self.marathon_client.list_tasks(self.app["name"])
+		containers_name = []
+		for task in tasks:
+			containers_name.append(get_container_name(task.id))
+		return containers_name
+
+	def avg_mem_usage(self, containers_name):
+		"""Return avg memmory usage of all containers in list containers_name
+		
+		@param list containers_name list containers name
+		@return float avg mem usage
+		"""
+		number_container = len(containers_name)
+		containers_name = ["'"+x+"'" for x in containers_name]
+		containers_name = ",".join(containers_name)
+		query = "select DERIVATIVE(memory_usage)  as memory_usage,container_name from stats where  time > now()-5m and  container_name in ("+containers_name+") group by time(10s),container_name limit "+str(number_container)
+		result = self.influx_client.query(query)
+		points = result[0]["points"]
+		sum_memory_usage = 0
+		for point in points:
+			sum_memory_usage += points[0][1]/self.app["mem"]*100
+		return sum_memory_usage / number_container
+
+	def avg_cpu_usage(self, containers_name):
+		"""Return avg cpu usage of all containers in list containers_name
+		
+		@param list containers_name list containers name
+		@return float avg cpu usage
+		"""
+		number_container = len(containers_name)
+		containers_name = ["'"+x+"'" for x in containers_name]
+		containers_name = ",".join(containers_name)
+		query = "select DERIVATIVE(cpu_cumulative_usage)  as cpu_usage,container_name from stats where  time > now()-5m and  container_name in ("+containers_name+") group by time(10s),container_name limit "+str(number_container)
+		result = self.influx_client.query(query)
+		points = result[0]["points"]
+		#return points[0][1]/1000000000/self.app["cpus"]*100
+		sum_cpu_usage = 0
+		for point in points:
+			sum_cpu_usage += points[0][1]/1000000000/self.app["cpus"]*100
+		return sum_cpu_usage / number_container
+
+def scale(self, delta):
 	"""sacle app_name (add or remove) delta intances
 	
 	@param string app_name name of application
 	@param int delta number intances add or remove
 	"""
-	marathon_client.scale_app(app_name, delta=delta)
-	logger.info("scaling "+app_name+": "+str(delta))
-	logger.info("waiting for config file haproxy.cfg...")
-	time.sleep(TIME['w_config_ha'])
-	logger.info("config file haproxy.cfg")
-	os.system("sudo ./servicerouter.py --marathon http://"+MARATHON["host"]+":"+MARATHON["port"]+" --haproxy-config /etc/haproxy/haproxy.cfg")
+	new_instance = self.app["instance"] + delta
+	if(new_instance > self.app['max_instances']):
+		new_instance = self.app['max_instances']
+	if(new_instance < self.app['min_instances']):
+		new_instance = self.app['min_instances']
+	if(new_instance != self.app["instance"]):
+		self.marathon_client.scale_app(self.app["name"], new_instance)
+		logger.info("Scaling "+self.app["name"]+" to: "+str(new_instance))
+		logger.info("Waiting for config file haproxy.cfg...")
+		time.sleep(self.config["TIME"]['w_config_ha'])
+		logger.info("Config file haproxy.cfg...")
+		os.system("sudo ./servicerouter.py --marathon http://"+config["MARATHON"]["host"]+":"+config["MARATHON"]["port"]+" --haproxy-config /etc/haproxy/haproxy.cfg")
+		self.app["instance"] = marathon_client.get_app(self.app["name"]).instances
+		logger.info("Sleep "+str(self.config["TIME"]['after_scale'])+"s...")
+		time.sleep(self.config["TIME"]['after_scale'])
 
-def check_rule(policie, value):
-	"""Check rule and scale if true
-	
-	@param models.Policie policies
-	@param float value value of metric type
-	@return boolean, True if violation else return False
-	"""
-	global number_instance
-	global app
-	if(policie["metric_type"] == 0):
-		if(value > policie["upper_threshold"]):
-			if(number_instance < app["max_instances"]):
-				scale(app["name"], policie["instances_in"])
-				number_instance = marathon_client.get_app(app["name"]).instances
-				logger.info("sleep "+str(TIME['in_up'])+"s...")
-				time.sleep(TIME['in_up'])
-			else:
-				logger.info("number intances is maximum")
-			return True
-		elif(value < policie["lower_threshold"]):
-			if(number_instance > app["min_instances"]):
-				scale(app["name"], 0-policie["instances_out"])
-				number_instance = marathon_client.get_app(app["name"]).instances
-				logger.info('scaled up done, current number intances: '+str(number_instance))
-				logger.info("sleep "+str(TIME['in_down'])+"s...")
-				time.sleep(TIME['in_down'])
-			else:
-				logger.info("number intances is minximum")
-			return True
-	if(policie["metric_type"] == 1):
-		pass
-	return False
 
+	def check_rule(self, policie, value, type):
+		"""Check rule and return number intances need scale
+		
+		@param models.Policie policies
+		@param tuple value values of metric
+		@return integer number intances need scale
+		"""
+		delta["up"] = 0
+		delta["down"] = 0
+		# Check upper_threshold
+		if(value[policie["metric_type"]] > policie["upper_threshold"]):
+			delta['up'] = policie["instances_in"]
+		# Check lower_threshold
+		if(value[policie["metric_type"]] < policie["lower_threshold"]):
+			delta['down'] = policie["instances_out"]
+		
+		return delta
+
+
+	def autoscaling(self):
+		while True:
+			try:
+				containers_name = self.get_containers_name(self.app["name"])
+				avg_cpu = self.avg_cpu_usage(containers_name)
+				avg_mem = self.avg_mem_usage(containers_name)
+				logger.info("Avg cpu usage:"+str(avg_cpu)+", avg memmory usage: "+ str(avg_mem))
+				rs_detal['up'] = 0
+				rs_detal['down'] = 10
+				for policie in self.app["policies"]:
+					delta = check_rule(self.policie, (cpu, mem))
+					if(rs_detal['up'] < delta['up']):
+						rs_detal['up'] = delta['up']
+					if(rs_detal['down'] > delta['down']):
+						rs_detal['down'] = delta['down']
+
+				if(rs_detal['up'] > 0):
+					 scale(self.app["name"], rs_detal['up'])
+				elif(rs_detal['down'] > 0):
+					scale(self.app["name"], 0-rs_detal['down'])
+			except Exception as e:
+				logger.exception(e)
+			finally:
+				time.sleep(self.config["TIME"]['monitor'])
 
 def main():
-	app_name = argv[1]
-	global app
-	global number_instance
-
-	conn = http.client.HTTPConnection(MARIA_RESTFUL['host'], MARIA_RESTFUL['port'])
-	conn.request("GET", "/app/name/"+app_name)
-	json_app = conn.getresponse().read().decode("utf-8")
-	app = json.loads(json_app)
-	conn.request("GET", "/app/name/"+app_name+"/policies")
-	json_policies = conn.getresponse().read().decode("utf-8")
-	policies = json.loads(json_policies)
-
-	number_instance = marathon_client.get_app(app_name).instances
-	while True:
-		try:
-			containers_name = get_containers_name(app_name)
-			avg_cpu = avg_cpu_usage(containers_name)
-			logger.info("avg cpu usage:"+str(avg_cpu))
-
-			for policie in policies:
-				if(check_rule(policie, avg_cpu)):
-					break
-		except Exception as e:
-			logger.exception(e)
-		finally:
-			time.sleep(TIME['monitor'])
+	scaler = Scaler(argv[1], CONFIG)
+	scaler.autoscaling()
 if __name__ == '__main__':
     main()
 
